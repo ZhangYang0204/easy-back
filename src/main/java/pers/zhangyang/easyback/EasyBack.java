7caa4695e6492803829c1e9e7a0192075508d20c
ZhangYang <3319527246@qq.com>
2022-08-11 15:54:20 +0800

    Initial commit

---
+++ src/main/java/pers/zhangyang/easyback/EasyBack.java
@@ -0,0 +1,18 @@
+package pers.zhangyang.easyback;
+
+import org.bstats.bukkit.Metrics;
+import pers.zhangyang.easylibrary.EasyPlugin;
+
+public class EasyBack extends EasyPlugin {
+    @Override
+    public void onOpen() {
+
+        // bStats统计信息
+        new Metrics(EasyBack.instance,16092);
+    }
+
+    @Override
+    public void onClose() {
+
+    }
+}
---
+++ src/main/java/pers/zhangyang/easyback/executor/ReloadPluginExecutor.java
@@ -0,0 +1,28 @@
+package pers.zhangyang.easyback.executor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import pers.zhangyang.easylibrary.base.ExecutorBase;
+import pers.zhangyang.easylibrary.util.MessageUtil;
+import pers.zhangyang.easylibrary.yaml.MessageYaml;
+
+import java.util.List;
+
+public class ReloadPluginExecutor extends ExecutorBase {
+    public ReloadPluginExecutor(@NotNull CommandSender sender, String commandName, @NotNull String[] args) {
+        super(sender, commandName, args);
+    }
+
+    @Override
+    protected void run() {
+        if (args.length!=0){
+            return;
+        }
+
+
+        List<String> list = MessageYaml.INSTANCE.getStringList("message.chat.reloadPlugin");
+        MessageUtil.sendMessageTo(this.sender, list);
+    }
+}
